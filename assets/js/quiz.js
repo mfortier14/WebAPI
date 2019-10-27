1d2134a4007ae6e362a73e6fd6fb72360ad2e656
Marissa Fortier <marissafortier@gmail.com>
2019-10-26 22:47:25 -0400

    adding in the loop to hide and show the appropriate questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,12 +1,17 @@
 var correctAnswer = "Correct!"
   var wrongAnswer = "Try Again!"
+  var questionA1 = $("quizdisplay");
+  var currentQuestionIndex = 0;
+  var titleA1 = $("question");
+  var choicesA1 = $("choice");
+
 
 
 // this will set the timer to 60 when the start quiz button is clicked and decrement the timer
   var timer = 60;
   var IntervalId;
-  var Clock = document.getElementById("timer");
-  document.getElementById("quiz").addEventListener("click", function() {
+  var Clock = $("timer");
+  $("quiz").on("click", function() {
       Clock.style.display = "block";
       IntervalId = setInterval(decrement, 1000);
       showQuestion();
@@ -48,3 +53,41 @@ var correctAnswer = "Correct!"
 // $(".buttons").on("click", function(){
 //     var btnResponse = $(this.text(questions[i]));
 // })
+
+function startQuiz() {
+    // hide the first question on the first screen
+    var startMessage = $("#message");
+    startMessage.attr("class", "hide");
+  
+    // show the questions that were hidden
+    questionAl.removeAttribute("class");
+
+    getQuestion();
+}  
+
+function getQuestion() {
+    // get current question object from array
+    var currentQuestion = quizdisplay[currentQuestionIndex];
+  
+    // update title with current question
+    var titleAl = $("question");
+    titleAl.text = currentQuestion.title;
+  
+    // clear out any old question choices
+    choicesAl.innerHTML = "";
+  
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+      // create new button for each choice
+      var choiceItem = $("button");
+      choiceItem.attr("class", "choice");
+      choiceItem.attr("value", choice);
+  
+      choiceNode.text = i + 1 + ". " + choice;
+  
+      // attach click event listener to each choice
+      choiceNode.onclick = questionClick;
+  
+      // display on the page
+      choicesAl.append(choiceNode);
+    })};
